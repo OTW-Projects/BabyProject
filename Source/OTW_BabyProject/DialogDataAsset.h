108f259a165dfb863c774914d2c27cee7d083b48
2300010 <94806803+2300010@users.noreply.github.com>
2025-08-07 14:07:06 -0400

    Pre-Migration to Rider
    DialogDataAsset class ADDED

---
+++ Source/OTW_BabyProject/DialogDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameData.h"
+#include "DialogDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OTW_BABYPROJECT_API UDialogDataAsset : public UGameData
+{
+	GENERATED_BODY()
+	
+};
