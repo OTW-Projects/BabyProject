dfdb464d52d466323210cdf215e5feb356aeea54
2300010 <94806803+2300010@users.noreply.github.com>
2025-08-07 16:35:18 -0400

    Rider IDE tests - Classes ADDED
    Data folder
    - QuestDataAsset & SceneDataAsset ADDED (INCOMPLETE)

---
+++ Source/OTW_BabyProject/Data/QuestDataAsset.cpp
@@ -0,0 +1,1 @@
+#include "QuestDataAsset.h"
---
+++ Source/OTW_BabyProject/Data/QuestDataAsset.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameData.h"
+#include "QuestDataAsset.generated.h"
+
+
+UCLASS()
+class OTW_BABYPROJECT_API UQuestDataAsset : public UGameData
+{
+	GENERATED_BODY()
+	
+};
